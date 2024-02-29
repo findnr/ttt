d3bb32cc659ac7588b3024e41865ae1c64b72b07
findnr <cym504875043@gmail.com>
2024-02-29 14:57:31 +0800

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo 123213;
 sudo apt-get install tcl tk expect
-ls /usr/bin | grep expect 
+expect -c "
+    spawn password
+    expect {
+        \"Password\" { send \"MING,1234\r\";}
+    }
+    expect {
+        \"Password\" { send \"MING,1234\r\";}
+    }
+expect eof"
 # while true
 # do
 #    ip addr
